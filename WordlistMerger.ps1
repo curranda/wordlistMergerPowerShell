b18203c21ed2509b29ce4c0f528699e2021cdbc3
kophen <kophen@SexyAlliance.com>
2023-04-01 11:35:48 -0400

    Create WordlistMerger.ps1

---
+++ WordlistMerger.ps1
@@ -0,0 +1,12 @@
+﻿$List1 = Get-Content "C:\Temp\List1.txt"
+$List2 = Get-Content "C:\Temp\List2.txt"
+
+foreach ($Word1 in $List1)
+{
+    foreach ($Word2 in $List2)
+    {
+        Write-Host "$Word1$word2" 
+        "$Word1$word2"  | Out-File -FilePath "C:\Temp\mergedList.txt" -NoClobber -Append
+        
+    }
+}
